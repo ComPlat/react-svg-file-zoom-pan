2691665f57457b501e81488cdae6b728b33c710a
jasonych99 <jasonych99@gmail.com>
2016-09-15 09:26:18 +0200

    add story SvgFileZoomPan

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
-import Button from '../index';
+import { storiesOf } from '@kadira/storybook';
+import SvgFileZoomPan from '../SvgFileZoomPan';
+import '../stylesheets/style.css';
 
-storiesOf('Button', module)
-  .add('default view', () => (
-    <Button onClick={ action('button clicked') }>Hello</Button>
+storiesOf('SvgFileZoomPan', module)
+  .add('single view', () => (
+    <div style={{ height: 300, border: '1px solid green' }}>
+      <SvgFileZoomPan svgPath="https://upload.wikimedia.org/wikipedia/commons/e/e4/World_Map_Blank_-_with_blue_sea.svg"
+        duration={300}
+        resize={true} />
+    </div>
   ))
-  .add('some emojies as the text', () => (
-    <Button>😀 😎 👍 💯</Button>
-  ))
-  .add('custom styles', () => {
-    const style = {
-      fontSize: 20,
-      textTransform: 'uppercase',
-      color: '#FF8833',
-    };
-    return (
-      <Button style={ style }>Hello</Button>
-    );
-  });
+  .add('multiple view', () => (
+    <div>
+      <div style={{ height: 300, border: '1px solid green' }}>
+        <SvgFileZoomPan svgPath="https://upload.wikimedia.org/wikipedia/commons/e/e4/World_Map_Blank_-_with_blue_sea.svg"
+          duration={300}
+          resize />
+      </div>
+      <div style={{ height: 300, border: '1px solid green' }}>
+        <SvgFileZoomPan svgPath="https://upload.wikimedia.org/wikipedia/commons/e/e4/World_Map_Blank_-_with_blue_sea.svg"
+          duration={300}
+          resize={true} />
+      </div>
+    </div>
+  ));
