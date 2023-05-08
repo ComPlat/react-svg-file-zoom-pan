3b49531c461409c1ca579afd66932f9851d3f0d6
Lan Le <lan.le@kit.edu>
2023-05-08 10:00:06 +0200

    update react-scrips to v5.0.1

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars, import/no-extraneous-dependencies, react/require-default-props */
 import React from 'react';
 import PropTypes from 'prop-types';
 import SvgWrapper from './components/svg_wrapper';
--- src/components/zoomable.js
+++ src/components/zoomable.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars, no-use-before-define */
 import * as d3 from 'd3';
 
 const zoomable = ({ el, duration }) => {
@@ -8,22 +9,22 @@ const zoomable = ({ el, duration }) => {
 
   const resetZoom = () => {
     svg.transition()
-        .duration(duration)
-        .call(
-          zoom.transform,
-          d3.zoomIdentity,
-        );
+      .duration(duration)
+      .call(
+        zoom.transform,
+        d3.zoomIdentity,
+      );
   };
 
   const svg = d3.select(el)
-                .append('svg')
-                .attr('id', 'svg-file-container')
-                .attr('width', '100%')
-                .attr('height', '100%')
-                .call(zoom)
-                .on('dblclick.zoom', resetZoom)
-                .append('g');
+    .append('svg')
+    .attr('id', 'svg-file-container')
+    .attr('width', '100%')
+    .attr('height', '100%')
+    .call(zoom)
+    .on('dblclick.zoom', resetZoom)
+    .append('g');
   return svg;
-}
+};
 
 export default zoomable;
