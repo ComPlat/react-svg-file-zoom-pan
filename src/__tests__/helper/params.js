5c0991ee44ce1524f99b87b439341b8c86534fb0
Lan Le <baolan2005@gmail.com>
2024-05-07 10:27:24 +0200

    upd: update d3js to version 7 (#47) (#51)

--- src/__tests__/helper/params.js
+++ src/__tests__/helper/params.js
@@ -2,7 +2,7 @@ const pathContent = 'https://upload.wikimedia.org/' +
               'wikipedia/commons/e/e4/' +
               'World_Map_Blank_-_with_blue_sea.svg';
 
-const svgContent = '<svg width="100" height="100">' +
+const svgContent = '<svg width="100%" height="100%">' +
             '<circle cx="50" cy="50" r="40" fill="yellow"></circle>' +
             '</svg>';
 
